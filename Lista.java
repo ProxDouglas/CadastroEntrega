638964e7f9c203edc203d318b7a7d597a37fb211
TromeBr <44294126+TromeBr@users.noreply.github.com>
2019-04-30 08:55:07 -0300

    Add files via upload

--- Lista.java
+++ Lista.java
@@ -3,17 +3,38 @@ import java.util.*;
 public class Lista{
     int tam;
     ArrayList<Lista> list = new ArrayList<>();
+    public Lista(){
+        setTamanho(0);
+    }
     
-    public void inserir(Lista lista){
-        list.add(lista);
+    public void inserir(Lista obj){
+        list.add(obj);
+        setTamanho(list.size()+1);
     }
     
     
-    public void deletar(Object obj) {
-        list.remove(obj);
+    public void deletar(int indice) {
+        list.remove(indice);
+        setTamanho(list.size()-1);
     }
     
-    public List<Lista> imprimir() {
+    
+    public ArrayList getLista(){
+     return this.list;   
+    }
+    
+    /*
+    public List<Lista> mostrar() {
         return new ArrayList<Lista>(list);
     }
+    */
+    
+    public void setTamanho(int i){
+    this.tam = i;
+    }
+    
+    public int getTamanho(){
+    return this.tam;
+    }
+    
 }
